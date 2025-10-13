a66dce16a15785abc216a6683d4440265b271bfb
issayass <issayas.yohannes@wsu.edu>
2025-10-12 23:40:59 -0700

    Unreadable
    Slop right now. I'll get to it in the morning but I'm trying to mimic the slideshow to the best of my ability

---
+++ Frontend/src/pages/FormsReviewer.jsx
@@ -0,0 +1,5 @@
+const FormsReviewer = () => {
+  return <h2>Forms Reviewer Page (later)</h2>;
+};
+
+export default FormsReviewer;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <section>
+      <h1>Welcome Home</h1>
+      <p>Images will be inserted here and information about the organization</p>
+    </section>
+  );
+};
+
+export default Home;
