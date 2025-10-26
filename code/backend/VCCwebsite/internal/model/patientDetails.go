fed468a1b3b1534ceb613ebf3a362ec6902c0d1f
Anise <cameron.casebolt17@gmail.com>
2025-10-26 00:33:20 -0700

    all structs for formatting done,will work on routes tomorrow

--- code/backend/VCCwebsite/internal/model/patientDetails.go
+++ code/backend/VCCwebsite/internal/model/patientDetails.go
@@ -3,6 +3,7 @@ package scripts
 type PatientDetails struct {
 	Name              string     `json:"name"`
 	Vitals            VitalSigns `json:"vitals"`
+	VisitReason       string     `json:"visit_reason"`
 	Context           string     `json:"context"`
 	Task              string     `json:"task"`
 	EncounterDuration string     `json:"encounter_duration"`
