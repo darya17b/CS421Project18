d34822d7404cc6374f9220bc6da5b78cd39c1dbc
Anise <cameron.casebolt17@gmail.com>
2025-11-05 15:17:24 -0800

    made medication card a slice and family history a slice

--- code/backend/VCCwebsite/internal/model/medicalHistory.go
+++ code/backend/VCCwebsite/internal/model/medicalHistory.go
@@ -1,11 +1,11 @@
 package scripts
 
 type MedicalHistory struct {
-	Medications         MedicationCard       `json:"medications"`
+	Medications         []MedicationCard     `json:"medications"`
 	Allergies           string               `json:"allergies"`
 	PastMedHis          PastMedicalHistory   `json:"past_med_his"`
 	PreventativeMeasure PreventativeMedicine `json:"preventative_measure"`
-	FamilyHist          FamilyHistory        `json:"family_hist"`
+	FamilyHist          []FamilyHistory      `json:"family_hist"`
 	SocialHist          SocialHistory        `json:"social_hist"`
 	SymptonReview       ReviewOfSymptoms     `json:"sympton_review"`
 }
